f932d4719511b06fbf6a50350f4a9a2d541e68c6
Shreyash Mishra <72780721+shre0079@users.noreply.github.com>
2026-04-03 16:20:20 +0530

    project init

---
+++ src/main/java/com/shreyash/zorvyn/finance_dashboard_backend/FinanceDashboardBackendApplication.java
@@ -0,0 +1,13 @@
+package com.shreyash.zorvyn.finance_dashboard_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FinanceDashboardBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FinanceDashboardBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/shreyash/zorvyn/finance_dashboard_backend/FinanceDashboardBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.shreyash.zorvyn.finance_dashboard_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FinanceDashboardBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
